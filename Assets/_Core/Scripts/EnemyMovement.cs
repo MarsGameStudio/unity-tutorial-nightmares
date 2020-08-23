7392f741f837d266c019d7a474490b6fcbbc8ecd
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-08-23 22:30:06 +0200

    feat: add enemy movement

---
+++ Assets/_Core/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+  [SerializeField] private NavMeshAgent navMeshAgent;
+
+  private Transform player;
+
+  private void Start()
+  {
+    player = GameObject.FindGameObjectWithTag("Player").transform;
+  }
+
+  private void Update()
+  {
+    navMeshAgent.SetDestination(player.position);
+  }
+}
