1522d906c95eb197ccad733efb8308ab37a56aab
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-08-23 22:10:05 +0200

    feat: add player camera

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+  [SerializeField] private Transform target;
+  [SerializeField] private float smoothness = 5f;
+
+  private Vector3 offset;
+
+  private void Start()
+  {
+    offset = transform.position - target.position;
+  }
+
+  private void FixedUpdate()
+  {
+    transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothness * Time.fixedDeltaTime);
+  }
+}
