937eb9374774e3044982b6f479b1057475ea64e9
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-08-25 00:47:56 +0200

    feat: add game over

---
+++ Assets/_Core/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+  [SerializeField] private float restartDelay = 5f;
+  [SerializeField] private Animator animatorController;
+
+  private PlayerHealth playerHealth;
+  private float restartCooldown;
+
+  private void Start()
+  {
+    playerHealth = FindObjectOfType<PlayerHealth>();
+  }
+
+  private void Update()
+  {
+    if (playerHealth.IsDead())
+    {
+      animatorController.SetTrigger("GameOver");
+
+      if ((restartCooldown += Time.deltaTime) >= restartDelay) SceneManager.LoadScene(0);
+    }
+  }
+}
