dfc9f38c3f2021a003694d8a08156581ee2c599f
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-08-25 00:26:59 +0200

    feat: add enemy spawner

---
+++ Assets/_Core/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+  [SerializeField] private GameObject[] enemies;
+  [SerializeField] private Transform[] points;
+  [SerializeField] private float timeBetweenSpawns;
+
+  private PlayerHealth playerHealth;
+
+  private void Awake()
+  {
+    InvokeRepeating("Spawn", timeBetweenSpawns, timeBetweenSpawns);
+  }
+
+  private void Start()
+  {
+    playerHealth = FindObjectOfType<PlayerHealth>();
+  }
+
+  private void Spawn()
+  {
+    if (playerHealth.IsDead()) return;
+
+    GameObject enemy = enemies[Random.Range(0, enemies.Length)];
+    Transform point = points[Random.Range(0, points.Length)];
+
+    Instantiate(enemy,
+      point.position,
+      point.rotation
+    );
+  }
+}
